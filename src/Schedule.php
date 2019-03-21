e8122b6ec94d893ccde2ec9b7d4713a23809fa6c
dimacros <dimacros@hotmail.com>
2019-03-21 06:51:24 -0500

    Quitar el cron job

--- src/Schedule.php
+++ src/Schedule.php
@@ -1,14 +1,14 @@
 <?php 
+
 namespace Dimacros;
+
 class Schedule 
 {
     public static function call() {
-        if ( !wp_next_scheduled ('notify_attendees_after_event') ) {
-            wp_schedule_event(time(), 'daily', 'notify_attendees_after_event');
-        }
+
     }
 
     public static function destroy() {
-        wp_clear_scheduled_hook('notify_attendees_after_event');       
+      
     }
 }
